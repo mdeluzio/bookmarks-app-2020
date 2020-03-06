296bf9f92e870c18f94aae79c602940058f46347
Mark DeLuzio <deluziomark@gmail.com>
2020-03-05 20:48:31 -0500

    added React Router to the application and fixed the API Key

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
   API_ENDPOINT: `https://tf-ed-bookmarks-api.herokuapp.com/v3/bookmarks`,
-  API_KEY: '$2a$10$ra1z0n2XnSnbMP/ipTMHeOqqrI7i8Rssm/z8MHTxgb7LamV7LpfXu',
+  API_KEY: '10$ABbBxBUwD.DF7DFP8L8L7eSakrR2xM.Is1l0Kh8teoyTzxY5OuAHS',
 }
